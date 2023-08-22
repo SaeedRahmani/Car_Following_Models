ea81ff2245b2cbd22f1021ea59cd757d256078ef
Saeed Rahmani <saeedrmd@gmail.com>
2023-08-22 12:50:33 +0200

    lstm work, lstm_all that trains over all trj not

--- src/lstm_all.py
+++ src/lstm_all.py
@@ -29,16 +29,18 @@ def read_cf_pair(dt, n):
 # x_lead, x_follow, v_follow, a_follow, a_lead, v_lead, t, size_lead, size_follow = read_cf_pair(data, 120)
 
 zarr_data = zarr.open('../data/valAV.zarr', mode='r')
-data = pd.DataFrame(data=dict(zip(['x_lead', 'x_follow', 'v_follow', 'a_follow', 'a_lead', 'v_lead', 't', 'size_lead', 'size_follow'], read_cf_pair(zarr_data, 120))))
+data = pd.DataFrame(data=dict(zip(['x_lead', 'x_follow', 'v_follow', 'a_follow', 'a_lead', 'v_lead', 't', 'size_lead', 'size_follow'], read_cf_pair(zarr_data, 3054))))
+
+for i in range (0, 50):
+    data = data.append(pd.DataFrame(data=dict(zip(['x_lead', 'x_follow', 'v_follow', 'a_follow', 'a_lead', 'v_lead', 't', 'size_lead', 'size_follow'], read_cf_pair(zarr_data, i)))))
 
 # Calculate spacing and relative speed
 data['spacing'] = data['x_lead'] - data['x_follow'] - data['size_lead']
 data['relative_speed'] = data['v_lead'] - data['v_follow']
 
 # Select features and target
-# features = data[['spacing', 'relative_speed', 'v_follow', 'v_lead', 'a_follow' ,'a_lead', 'x_lead']]
-features = data[['spacing', 'relative_speed']]
-target = data['a_follow']
+features = data[['spacing', 'relative_speed', 'v_follow', 'v_lead', 'a_follow' ,'a_lead', 'x_lead']]
+target = data['x_follow']
 
 # Normalize data
 scaler = MinMaxScaler()
@@ -48,11 +50,11 @@ features = scaler.fit_transform(features)
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
 # Convert data to PyTorch tensors
-X_train_tensor = torch.FloatTensor(X_train).unsqueeze(2)
+X_train_tensor = torch.FloatTensor(X_train)
 y_train_tensor = torch.FloatTensor(y_train.values)
-X_test_tensor = torch.FloatTensor(X_test).unsqueeze(2)
+X_test_tensor = torch.FloatTensor(X_test)
 y_test_tensor = torch.FloatTensor(y_test.values)
-
+ 
 # 2. Model Definition
 class CarFollowingLSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
@@ -69,7 +71,7 @@ class CarFollowingLSTM(nn.Module):
         out = self.linear(out[:, -1, :]) 
         return out
 
-input_dim = 1
+input_dim = X_train.shape[1]
 hidden_dim = 32
 num_layers = 2
 output_dim = 1
@@ -104,3 +106,34 @@ y_pred = test_outputs.detach().numpy()
 y_true = y_test_tensor.numpy()
 mse = mean_squared_error(y_true, y_pred)
 print(f'MSE on test set: {mse}')
+
+
+# Plotting the results
+import matplotlib.pyplot as plt
+
+x_lead = leader_data['x_lead'].values
+x_follow_actual = follower_data['x_follow'].values
+
+# LSTM model prediction
+# Prepare input features for LSTM prediction (based on the features you've used for training)
+input_features = follower_data[['feature1', 'feature2', ...]].values 
+# Normalize and reshape the input features
+input_features_scaled = scaler.transform(input_features) # Assuming 'scaler' was used to normalize the training data
+input_features_tensor = torch.FloatTensor(input_features_scaled).unsqueeze(0) # Add batch dimension
+# Predict using the trained LSTM model
+model.eval() # Set the model to evaluation mode
+with torch.no_grad():
+    predictions = model(input_features_tensor)
+# Convert predictions to NumPy array
+x_follow_pred = predictions.numpy().squeeze()
+
+# Plotting
+plt.figure(figsize=(12, 6))
+plt.plot(x_lead, label='Leader Position', color='blue')
+plt.plot(x_follow_actual, label='Follower Actual Position', color='green')
+plt.plot(x_follow_pred, label='Follower Predicted Position', color='red', linestyle='dashed')
+plt.xlabel('Time Step')
+plt.ylabel('Position')
+plt.legend()
+plt.title('Leader and Follower Positions Over Time')
+plt.show()
